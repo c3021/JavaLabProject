3b3014bb8738cd7d8935e0ff74db219e2e3628a7
c3021 <c3021@outlook.com>
2015-03-15 11:52:09 +0800

    add lab5

--- JavaLabProject/src/blog/Blog.java
+++ JavaLabProject/src/blog/Blog.java
@@ -1,25 +1,33 @@
 package blog;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 import base.Post;
 import base.User;
 
-public class Blog {
-	
+public class Blog implements Serializable {
+
 	private User user;
 	private ArrayList<Post> allPosts;
-	
+
 	/**
 	 * Constructor
+	 * 
 	 * @param user
 	 */
 	public Blog(User user) {
 		this.user = user;
-		allPosts = new ArrayList<Post>();//initialize the ArrayList
+		allPosts = new ArrayList<Post>();// initialize the ArrayList
 	}
-	
+
 	/**
 	 * 
 	 * @return
@@ -53,52 +61,98 @@ public class Blog {
 	}
 
 	/**
+	 * 
+	 * @param pathfile
+	 */
+	public void save(String pathfile) {
+		try {
+			FileOutputStream fos = new FileOutputStream(pathfile);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(this);
+			//oos.writeObject(user);
+			//oos.writeObject(allPosts);
+			oos.close();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @param pathfile
+	 */
+	public void load(String pathfile) {
+		try {
+			FileInputStream fis = new FileInputStream(pathfile);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Blog temp = (Blog) ois.readObject();
+			this.user = temp.user;
+			this.allPosts = temp.allPosts;
+			//this.user = (User) ois.readObject();
+			//this.allPosts = (ArrayList<Post>) ois.readObject();
+			ois.close();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Wait! There are something wrong. I cannot find the file..");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
 	 * create a new post in your blog
+	 * 
 	 * @param post
 	 */
-	public void post(Post post)	{
-		//add a post to your blog
+	public void post(Post post) {
+		// add a post to your blog
 		int num = allPosts.size();
 		allPosts.add(post);
-		//print a message if succeed
+		// print a message if succeed
 		if (++num == allPosts.size()) {
 			System.out.println("A new Post:\n" + post);
 		}
 	}
-	
+
 	/**
 	 * Delete a post from the blog (Index starts from 1)
 	 */
 	public void delete(int index) {
-		if (--index > -1 && index < allPosts.size()){
+		if (--index > -1 && index < allPosts.size()) {
 			allPosts.remove(index);
-		}
-		else {
+		} else {
 			System.out.println("Illegal deletion. (List index out of bound)");
 		}
 	}
-	
+
 	/**
 	 * list all posts in the blog
 	 */
 	public void list() {
 		System.out.println("Current posts: ");
 		for (int i = 0; i < allPosts.size(); i++) {
-		System.out.println("Post[" + (i + 1) + "]: " + allPosts.get(i));
+			System.out.println("Post[" + (i + 1) + "]: " + allPosts.get(i));
 		}
-		
+
 		/*
-		for (Post p : allPosts){
-			System.out.println(p);
-		}
-		*/
+		 * for (Post p : allPosts){ System.out.println(p); }
+		 */
 	}
-	
-	public void search (int month, String someone) {
+
+	public void search(int month, String someone) {
 		Calendar cal = Calendar.getInstance();
-		//search from all posts
-		for (Post p: allPosts) {
-			//get the current post's month (note that Calendar.Month starts with 0 (Jan), not 1)
+		// search from all posts
+		for (Post p : allPosts) {
+			// get the current post's month (note that Calendar.Month starts
+			// with 0 (Jan), not 1)
 			cal.setTime(p.getDate());
 			if ((month - 1) == cal.get(Calendar.MONTH)) {
 				if (p.contains(someone))
@@ -106,14 +160,16 @@ public class Blog {
 			}
 		}
 	}
+
 	/**
 	 * 
 	 */
 	@Override
 	public String toString() {
-		return user.toString() + "\n" + allPosts.size() + "post(s) in this blog";//allPosts.toString() ;
+		return user.toString() + "\n" + allPosts.size()
+				+ "post(s) in this blog";// allPosts.toString() ;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -135,7 +191,7 @@ public class Blog {
 			return false;
 		return true;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
